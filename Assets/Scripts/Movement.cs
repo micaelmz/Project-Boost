b3203de8fd0318693a9cdbef430daaa79b4e8102
Micael <micaelmuniz6@gmail.com>
2024-03-17 02:05:32 -0300

    adicionando som ao acionar o thrust do foguete

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,12 +7,13 @@ public class Movement : MonoBehaviour {
     [SerializeField] private float verticalThrustForce = 1000f;
     [SerializeField] private float horizontalThrustForce = 100f;
 
-
+    private AudioSource audioSource;
     private Rigidbody rocketRigidyBody;
 
     // Start is called before the first frame update
     void Start() {
         rocketRigidyBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -23,14 +24,22 @@ public class Movement : MonoBehaviour {
 
 
     void ProcessVerticalInput() {
-        if (Input.GetAxis("Jump") > 0.1f) {
+        if (Input.GetAxis("Jump") != 0) {
             rocketRigidyBody.AddRelativeForce(Time.deltaTime * verticalThrustForce * Vector3.up);
+
+            if (!audioSource.isPlaying) {
+                audioSource.Play();
+            }
+        }
+
+        if (Input.GetAxis("Jump") == 0 && audioSource.isPlaying) {
+            audioSource.Stop();
         }
     }
 
     void ProcessHorizontalInput() {
         var horizontalAxis = Input.GetAxis("Horizontal");
-        
+
         if (horizontalAxis != 0) {
             rocketRigidyBody.freezeRotation = true; // travando o controle para que manualmente seja alterado
             transform.Rotate(
@@ -41,5 +50,4 @@ public class Movement : MonoBehaviour {
             rocketRigidyBody.freezeRotation = false; // liberando para que a fisica do jogo possa modificar como quiser novamente
         }
     }
-    
 }
