1c9a443329cb9a62ea1424d9bc146005e2e7e074
Micael <micaelmuniz6@gmail.com>
2024-03-17 02:36:17 -0300

    Revert "otimizando o codigo adicionando conceitos de estados, para nao ficar verificando a cada frame por updates que o estado anterior nao foi alterado"
    This reverts commit 09c00ec227a8b1e6ad87501930744f82cb5eefa7.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,41 +7,33 @@ public class Movement : MonoBehaviour {
     [SerializeField] private float verticalThrustForce = 1000f;
     [SerializeField] private float horizontalThrustForce = 100f;
 
-    // Components
     private AudioSource audioSource;
     private Rigidbody rocketRigidyBody;
-    
-    // States
-    private bool previousJumpState = false;
 
+    // Start is called before the first frame update
     void Start() {
         rocketRigidyBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
     }
 
+    // Update is called once per frame
     void Update() {
         ProcessVerticalInput();
         ProcessHorizontalInput();
     }
-    
+
+
     void ProcessVerticalInput() {
-        bool currentJumpState = Input.GetAxis("Jump") > 0.1f;
-        
-        // Can be jumping (true) or not jumping (false)
-        if (currentJumpState != previousJumpState) {
-            // is jumping
-            if (currentJumpState) {
-                rocketRigidyBody.AddRelativeForce(Time.deltaTime * verticalThrustForce * Vector3.up);
-                if (!audioSource.isPlaying) {
-                    audioSource.Play();
-                }
+        if (Input.GetAxis("Jump") != 0) {
+            rocketRigidyBody.AddRelativeForce(Time.deltaTime * verticalThrustForce * Vector3.up);
+
+            if (!audioSource.isPlaying) {
+                audioSource.Play();
             }
-            // Is not jumping
-            else {
-                audioSource.Stop();
-            }
-            
-            previousJumpState = currentJumpState;
+        }
+
+        if (Input.GetAxis("Jump") == 0 && audioSource.isPlaying) {
+            audioSource.Stop();
         }
     }
 
